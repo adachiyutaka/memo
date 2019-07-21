d465cd4ca85b1e71cad64e36475905e67d501a49
Yutaka Adachi <adachi.yutaka@gmail.com>
2019-07-21 18:19:27 +0900

    first commit

---
+++ app/src/main/java/com/example/e28/memo/Memo.java
@@ -0,0 +1,27 @@
+package com.example.e28.memo;
+
+import java.util.Date;
+
+import io.realm.RealmList;
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+/**
+ * Created by User on 2019/07/14.
+ */
+
+public class Memo extends RealmObject {
+    @PrimaryKey
+    public long id;
+    public Date createdAt;
+    public Date updatedAt;
+    public String text;
+    public String imagePath;
+    public boolean isTagged;
+    public RealmList<Tag> tagList;
+    public boolean isTodo;
+    public RealmList<Todo> toDoList;
+    public boolean isHighlight;
+    public boolean isChecked;
+    public boolean isTrash;
+}
