5954ccd18b99c1fea42bdea2a9966b83a08b2f24
Yutaka Adachi <adachi.yutaka@gmail.com>
2019-07-31 22:39:32 +0900

    making preference

--- app/src/main/java/com/example/e28/memo/screen/manage/ManageActivity.java
+++ app/src/main/java/com/example/e28/memo/screen/manage/ManageActivity.java
@@ -1,18 +1,44 @@
 package com.example.e28.memo.screen.manage;
 
 import android.os.Bundle;
+import android.preference.PreferenceScreen;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.preference.PreferenceFragmentCompat;
 import android.support.v7.widget.Toolbar;
 
 import com.example.e28.memo.R;
 
-public class ManageActivity extends AppCompatActivity {
+public class ManageActivity extends AppCompatActivity implements PreferenceFragmentCompat.OnPreferenceStartScreenCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_trash);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setContentView(R.layout.activity_settings);
+
+        // Toolbarの設定
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // savedInstanceStateがnullでない場合は前回のFragmentが自動で復元されるのでnullの場合のみ処理
+        if (savedInstanceState == null) {
+            // トップ画面のFragmentを表示
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, SettingsFragment.newInstance("preference_root"))
+                    .commit();
+        }
+    }
+
+    // PreferenceScreenがクリックされた時に呼び出されます
+    @Override
+    public boolean onPreferenceStartScreen(PreferenceFragmentCompat caller, PreferenceScreen pref) {
+        // Fragmentの切り替えと、addToBackStackで戻るボタンを押した時に前のFragmentに戻るようにする
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, SettingsFragment.newInstance(pref.getKey()))
+                .addToBackStack(null)
+                .commit();
+        return true;
     }
 }
